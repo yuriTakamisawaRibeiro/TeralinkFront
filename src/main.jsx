06fddfeee98b519f9c794c18b1f143511931e591
Rirfit <lilrirfit@gmail.com>
2024-04-01 17:21:56 -0300

    Revert "Pagina login"
    This reverts commit e305b60926f0b3f4f23813ec89e32539e7ecdc8e.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GlobalStyle from './styles/global';
 import { SigninPatient } from './pages/SignInPatient';
-import { LoginPatient} from './pages/LoginPatient';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle/>
-    <LoginPatient/>
-    
+    <SigninPatient />
   </React.StrictMode>,
 )
