259fec8e4d2777b40d137c9fdffbd8cde0f6f716
yuritakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2024-05-15 10:46:01 -0300

    userPatientProfile

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,11 @@ import GlobalStyle from './styles/global';
 import { SignInPatient } from './pages/SignInPatient';
 import { SignUpPatient } from './pages/SignUpPatient';
 import { Home } from './pages/Home';
+import { UserPatientProfile } from './pages/UserPatientProfile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle/>
-    <Home />
+    <UserPatientProfile />
   </React.StrictMode>,
 )
---
+++ src/pages/UserPatientProfile/index.jsx
@@ -0,0 +1,26 @@
+import { Header } from "../../components/Header"
+import { ProfileOption } from "../../components/ProfileOption"
+import { Container, Background1, DataArea, Title,Area } from "./styles"
+import background1 from "../../assets/background1.svg"
+
+
+export const UserPatientProfile = () => {
+
+
+    return (
+        <Container>
+            <Background1 background={background1} />
+            <Header />
+
+            <DataArea style={{ alignItems: 'center' }}>
+                <Area>
+                <Title>Meus dados</Title>
+
+                <ProfileOption title="Minhas consultas" subtitle="Histórico de consultas" />
+                </Area>
+                <ProfileOption title="Informações pessoais" subtitle="Nome completo, email e CPF" />
+                <ProfileOption title="Credenciais" subtitle="Gerenciar senha" />
+            </DataArea>
+        </Container>
+    )
+}
