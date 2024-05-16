cdf36e77b313700af78c5cd4913f26b4914f0d27
Humopz <142692784+Humopz@users.noreply.github.com>
2024-05-16 09:04:15 -0300

    Rotas funcionando

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GlobalStyle from './styles/global';
 
+import { Routes } from './routes';
 
-import { SignInPatient } from './pages/SignInPatient';
-import { SignUpPatient } from './pages/SignUpPatient';
-import { Home } from './pages/Home';
-import { UserPatientProfile } from './pages/UserPatientProfile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle/>
-    <Home />
+    <Routes />
   </React.StrictMode>,
 )
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { SignInPatient } from './pages/SignInPatient';
-import { SignUpPatient } from './pages/SignUpPatient';
-import { UserPatientProfile } from './pages/UserPatientProfile';
+import { Home } from "../pages/Home"
+import { SignInPatient } from '../pages/SignInPatient';
+import { SignUpPatient } from '../pages/SignUpPatient';
+import { UserPatientProfile } from '../pages/UserPatientProfile';
 
 export function AppRoutes() {
     return (
